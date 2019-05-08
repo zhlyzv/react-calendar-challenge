2280fc4a8643e9125b2ff081ac706a76f60c8bd4
Alexander Zhelyazov <azhelyazov@gmail.com>
2019-05-08 03:11:32 +0100

    update range logic

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,14 +1,14 @@
 import React, { Component, Fragment } from 'react';
 import {
-    getMonth,
     isToday,
-    getDaysInMonth,
     eachDay,
     startOfMonth,
     endOfMonth,
     getDay,
     format,
     subMonths,
+    addMonths,
+    isSameMonth,
 } from 'date-fns';
 import styled from 'styled-components';
 import GlobalStyle from '../styles';
@@ -57,43 +57,47 @@ const Day = styled.div`
     align-items: start;
     padding: 20px;
     font-size: 1.2rem;
-    background-color: ${props => (props.isToday ? '#faf0e9' : '#bfccc847')};
+    background-color: ${props =>
+        props.isToday ? '#faf0e9' : props.isSameMonth ? '#bfccc847' : '#fff'};
 `;
 
-const Dday = ({ day, isToday }) => (
-    <Day isToday={isToday} className='Day'>
-        {day}
-    </Day>
-);
-
-// const monthNames = [
-//     'January',
-//     'February',
-//     'March',
-//     'April',
-//     'May',
-//     'June',
-//     'July',
-//     'August',
-//     'September',
-//     'October',
-//     'November',
-//     'December',
-// ];
 const today = new Date();
-const todayMonthRange = eachDay(startOfMonth(today), endOfMonth(today));
+const currentRange = eachDay(startOfMonth(today), endOfMonth(today));
 
-// Check if start of month is on a Sunday
-// If not, add items from last month till first Sunday
-const dayAtStart = getDay(startOfMonth(today));
-const daysToGetFromLastMonth = 6 - dayAtStart;
-const lastMonthDays = getDaysInMonth(4);
-console.log(endOfMonth(today));
-console.log(subMonths(endOfMonth(today), 1));
+const padMonthRange = monthRange => {
+    const testDay = endOfMonth(monthRange[0]);
+    // Get padding for previous month
+    const paddingPrev = 6 - getDay(startOfMonth(monthRange[0]));
+    const prevMonthRange = eachDay(
+        startOfMonth(subMonths(testDay, 1)),
+        endOfMonth(subMonths(testDay, 1))
+    );
+    const daysPaddingPrev = prevMonthRange.slice(-paddingPrev);
+    // Get padding for next mounth
+    const paddingNext = 6 - getDay(testDay);
+    const nextMonthRange = eachDay(
+        startOfMonth(addMonths(testDay, 1)),
+        endOfMonth(addMonths(testDay, 1))
+    );
+    const daysPaddingNext = nextMonthRange.slice(0, paddingNext);
 
-const days = todayMonthRange.map(day => {
+    return [...daysPaddingPrev, ...monthRange, ...daysPaddingNext];
+};
+
+const currentMonthRange = padMonthRange(currentRange);
+
+// eslint-disable-next-line react/destructuring-assignment
+const DayWrapper = props => <Day {...props}>{props.day}</Day>;
+const days = currentMonthRange.map(day => {
     const formattedDay = format(day, 'D');
-    return <Dday key={day.toISOString()} day={formattedDay} isToday={isToday(day)} />;
+    return (
+        <DayWrapper
+            key={day.toISOString()}
+            day={formattedDay}
+            isToday={isToday(day)}
+            isSameMonth={isSameMonth(day, today)}
+        />
+    );
 });
 
 class Calendar extends Component {
